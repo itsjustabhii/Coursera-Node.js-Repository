4381456fe28e98bfeb5b832486e7c958cc897092
itsjustabhii <itsjustabhii@gmail.com>
2023-02-03 10:21:40 +0530

    modified user schema using plugin and passport-local

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -1,20 +1,15 @@
 import mongoose from "mongoose";
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
+//Will automatically add username and password, will salt and hash the password
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
