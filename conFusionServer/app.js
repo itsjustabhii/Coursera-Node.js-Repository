9d100b2c92c0e119c9de26f0a6cc1680eb2478c6
itsjustabhii <itsjustabhii@gmail.com>
2023-02-17 12:07:04 +0530

    added /favorite endpoint

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,6 +16,7 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 var uploadRouter = require("./routes/uploadRouter");
+var favoriteRouter = require("./routes/favoriteRouter");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -37,6 +38,7 @@ app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use("/imageUpload", uploadRouter);
+app.use("/favorite", favoriteRouter);
 
 app.use(auth);
 
