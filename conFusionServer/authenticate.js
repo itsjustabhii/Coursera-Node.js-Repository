3234c2b7bf9a0ea48a93c2826c1f5cf30548afe0
itsjustabhii <itsjustabhii@gmail.com>
2023-02-17 12:09:07 +0530

    added more authentication

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -4,7 +4,6 @@ var User = require("./models/user");
 var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
 var jwt = require("jsonwebtoken");
-
 var config = require("./config");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
@@ -34,5 +33,13 @@ exports.jwtPassport = passport.use(
     });
   })
 );
-
 exports.verifyUser = passport.authenticate("jwt", { session: false }); // not creating session
+
+exports.verifyAdmin = (req, res, next) => {
+  if (req.user.admin) next();
+  else {
+    var err = new Error("You are not authorized to perform this operation!");
+    err.status = 403;
+    return next(err);
+  }
+};
